8dd5820e752062b5d29c2f234f923f5d228d7a85
josephdurand25 <durandjosephadji25@gmail.com>
2024-07-25 18:19:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+//les opérateurs
+
+$x=3;
+
+$y=6;
+
+// >=
+// <=
+// !=
+// !
+
+// incrémentation
+
+$x ++; // +1
+
+$y+=5; //
+
+// décrementation
+
+$y-=4;
+
+
+echo $y;
+
+
---
+++ traitements/page1.php
@@ -0,0 +1,17 @@
+<?php
+
+function NomDeLaFonction($param1, $param2){
+    // instruction
+}
+
+//les opérateurs
+
+$x=3;
+$y=6;
+// >
+
+if($x > $y ){
+    echo  " x > y";
+}else{
+    echo  " x <y" ;
+}
